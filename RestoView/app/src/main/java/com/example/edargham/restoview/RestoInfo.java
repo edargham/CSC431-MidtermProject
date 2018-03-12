33af055ce04d532e5f1bf5303a5ef0f20f17d5e1
edargham <edargham@hotmail.com>
2018-03-12 19:17:10 +0200

    Added Back Button, Submittable

--- RestoView/app/src/main/java/com/example/edargham/restoview/RestoInfo.java
+++ RestoView/app/src/main/java/com/example/edargham/restoview/RestoInfo.java
@@ -1,14 +1,28 @@
 package com.example.edargham.restoview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestoInfo extends AppCompatActivity {
+
+    View.OnClickListener ocls = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resto_info);
+
+        Button backButton = findViewById(R.id.backbutton);
+        backButton.setOnClickListener(ocls);
     }
 }
